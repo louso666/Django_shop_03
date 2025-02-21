2a683bd12d2a579ef80f88c89a89e120541832ed
Юрий Лукашевич <LukashevichYA@srgroup.ru>
2025-02-21 20:39:52 +0300

    Add STATIC_ROOT fix

--- config/settings.py
+++ config/settings.py
@@ -129,13 +129,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
