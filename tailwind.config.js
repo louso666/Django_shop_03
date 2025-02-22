f5d3ce203e06056b09fb3b25ab7ae86264e230b0
Юрий Лукашевич <LukashevichYA@srgroup.ru>
2025-02-22 12:12:32 +0300

    lang

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./templates/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#15801b',
+        'secondary': '#6d7d6c',
+      },
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
